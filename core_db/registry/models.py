ce45c051321c6195785f1046ab615a4718f376f1
James Barwick <jbarwick@me.com>
2025-01-03 08:54:11 +0800

    Deployspec Test

--- core_db/registry/models.py
+++ core_db/registry/models.py
@@ -13,6 +13,10 @@ class RegistryModel(Model):
         kwargs = self.convert_keys(**kwargs)
         super().__init__(*args, **kwargs)
 
+    def convert_keys(self, **kwargs) -> dict:
+        # Convert snake_case and kebab-case keys to PascalCase keys
+        return {self._convert_key(k): v for k, v in kwargs.items()}
+
     def _convert_key(self, key):
         # Convert snake_case and kebab-case keys to PascalCase keys
         attributes = self.get_attributes()
@@ -22,9 +26,20 @@ class RegistryModel(Model):
         pascal_case_key = "".join(word.capitalize() for word in words)
         return pascal_case_key
 
-    def convert_keys(self, **kwargs) -> dict:
-        # Convert snake_case and kebab-case keys to PascalCase keys
-        return {self._convert_key(k): v for k, v in kwargs.items()}
+    def convert_keys_alt(self, **kwargs) -> dict:
+        # Convert snake_case and kebab-case keys to PascalCase keys.  Does NOT handle camelCase
+        rv = {}
+        for k, v in kwargs.items():
+            nk = self._convert_key(k)
+            if isinstance(v, dict):
+                rv[nk] = self.convert_keys(**v)
+            elif isinstance(v, list):
+                for i, item in enumerate(v):
+                    if isinstance(item, dict):
+                        v[i] = self.convert_keys(**item)
+            else:
+                rv[nk] = v
+        return rv
 
 
 class ExtendedMapAttribute(MapAttribute):
