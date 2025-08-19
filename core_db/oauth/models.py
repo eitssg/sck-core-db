de2f10eb674a48600754ac0629a16c983eabfdb8
James Barwick <jbarwick@me.com>
2025-08-19 11:51:35 +0800

    Spelling Errors

--- core_db/oauth/models.py
+++ core_db/oauth/models.py
@@ -7,11 +7,11 @@ class Authorizations(DatabaseTable):
     class Meta(DatabaseTable.Meta):
         pass
 
-    codr = UnicodeAttribute(hash_key=True, attr_name="Code")
+    code = UnicodeAttribute(hash_key=True, attr_name="Code")
     client_id = UnicodeAttribute(attr_name="Client")
     user_id = UnicodeAttribute(attr_name="UserId")
     redirect_url = UnicodeAttribute(attr_name="RedirectUrl")
-    scopr = UnicodeAttribute(attr_name="Scope")
+    scope = UnicodeAttribute(attr_name="Scope")
     jwt_token = UnicodeAttribute(attr_name="JwtToken")
     expires_at = UTCDateTimeAttribute(null=True, attr_name="ExpiresAt")
 
